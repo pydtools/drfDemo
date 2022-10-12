d179a5514ce9b3424feb13a7b657fe2f5b31d1ec
tphyhFighting <2363176358@qq.com>
2022-10-12 18:31:03 +0800

    add: owner UserSerializer

--- mysite/snippets/serializers.py
+++ mysite/snippets/serializers.py
@@ -23,6 +23,18 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 #         return instance
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'language', 'style')
+        fields = ('id', 'title', 'code', 'language', 'style', 'owner')
+
+
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
